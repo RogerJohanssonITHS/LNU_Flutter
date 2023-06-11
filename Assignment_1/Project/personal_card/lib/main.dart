404b4b698146bd3ad72144246bae7cda9dc69c8a
RogerJohanssonITHS <89128014+RogerJohanssonITHS@users.noreply.github.com>
2023-06-11 16:06:49 +0200

    More card details.

--- Assignment_1/Project/personal_card/lib/main.dart
+++ Assignment_1/Project/personal_card/lib/main.dart
@@ -1,27 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
+/// main application widget
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Personal Card';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First Flutter',
+      title: _title,
       home: Scaffold(
-        appBar: AppBar(
-            title: const Text('Personal Card'),
-            leading: const Icon(Icons.flutter_dash)),
-        body: Center(
-          child: Text(
-            'Hello Flutter!',
-            textDirection: TextDirection.ltr,
-            style: GoogleFonts.acme(fontSize: 32),
-          ),
-        ),
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatelessWidget(),
       ),
+    );
+  }
+}
+
+/// stateless widget that the main application instantiates
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipOval(
+                child: Image.asset(
+                  ('assets/images/_E_R0553_edit.jpg'),
+                  width: 150,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Text(
+              ('Roger Johansson'),
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.aladin(fontSize: 32),
+            ),
+            Card(
+              color: Colors.redAccent,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    ('Student'),
+                    textDirection: TextDirection.ltr,
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.bitter(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    ('Roger Johansson'),
+                    textDirection: TextDirection.ltr,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.bitter(fontSize: 32),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
